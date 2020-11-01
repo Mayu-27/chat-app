9a33d4d39d13183457c97e940c6c31559576ac43
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2020-11-01 12:18:42 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  has_many: room_users
-  has_many: rooms, thorough: :room_users
-
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
